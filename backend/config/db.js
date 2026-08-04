b2203b803f6cd72788bce23089218d53144118dd
teoscrvn-glitch <teoscrvn@gmail.com>
2026-08-04 14:50:35 +0700

    Fix fallback DB host and port

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,11 +2,11 @@ const mysql = require('mysql2/promise');
 require('dotenv').config();
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT || 3306,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: process.env.DB_HOST || 'mysql-b4e858-nooblemon3030-8526.aivencloud.com',
+  port: process.env.DB_PORT || 23993,
+  user: process.env.DB_USER || 'avnadmin',
+  password: process.env.DB_PASSWORD, // Nhớ chắc chắn đã điền DB_PASSWORD trên Render
+  database: process.env.DB_NAME || 'defaultdb',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
